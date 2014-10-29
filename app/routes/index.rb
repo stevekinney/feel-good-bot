29c94c0252b64348ac4e8fb6fdd229612df30e72
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-10-29 11:27:22 -0600

    Add affirmation route /a6ftcruton

--- app/routes/index.rb
+++ app/routes/index.rb
@@ -2,4 +2,8 @@ class FeelGoodBot < Sinatra::Base
   get '/' do
     "Today is going to be a wonderful day."
   end
+
+  get '/a6ftcruton' do
+    "In death, we have a name. His name is a six foot cruton."
+  end
 end
