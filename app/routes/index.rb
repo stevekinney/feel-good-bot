154a8ef7333718ec2560a2cbe9f56ffb078c213e
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-29 11:50:16 -0600

    added links to other pages on home page

--- app/routes/index.rb
+++ app/routes/index.rb
@@ -1,5 +1,5 @@
 class FeelGoodBot < Sinatra::Base
   get '/' do
-    "Today is going to be a wonderful day."
+    "Today is going to be a wonderful day." +" "  + "<a href='/affirmation'>Affirmations</a>" + " " + "<a href='/fortune'>Fortune</a>" + " " + "<a href='/inspirational_quote'>Ispirational Quotes</a>"
   end
 end
